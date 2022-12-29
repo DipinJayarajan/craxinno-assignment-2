a9878f4cee5b16be790bb7cc1e99390288522fea
Dipin Nair <95134716+DipinJayarajan@users.noreply.github.com>
2022-12-29 20:47:57 +0530

    Reviewing the code

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Content.css"
-import Bg from "../assets/bg.png"
+import Bg from "../assets/Bg.png"
 import Card1 from "../assets/icon.png"
 import Card2 from "../assets/icon(1).png"
 import Card3 from "../assets/icon(2).png"
